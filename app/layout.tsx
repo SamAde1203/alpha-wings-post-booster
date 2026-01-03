2c27c6297e65ca86b32c869ee7f83cc5aa9f6f56
Sam Adeyemi <sammyadeyemi@gmail.com>
2026-01-03 21:44:59 +0000

    Add Google Analytics tracking (G-8MJS7FPV80)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css"
 
 const geistSans = Geist({
@@ -56,6 +57,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-b from-blue-50 to-white`}
       >
         {children}
+        <GoogleAnalytics gaId="G-8MJS7FPV80" />
       </body>
     </html>
   )
